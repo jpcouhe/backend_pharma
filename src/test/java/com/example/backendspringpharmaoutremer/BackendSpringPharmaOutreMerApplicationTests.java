d93298989d1fc9e49efbf18458c6b22e9fb77a88
soj i <jp.couhe.dev@gmail.com>
2023-03-11 18:24:51 +0100

    init project

---
+++ src/test/java/com/example/backendspringpharmaoutremer/BackendSpringPharmaOutreMerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendspringpharmaoutremer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringPharmaOutreMerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
